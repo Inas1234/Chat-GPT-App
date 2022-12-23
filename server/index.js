6c1655a14fb839d73e8b223fed05c1a32570c2bf
Inas1234 <inas.malkic@yahoo.com>
2022-12-23 19:58:38 +0100

    I beg of you work

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 const configuration = new openai.Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 
 const client = new openai.OpenAIApi(configuration);
