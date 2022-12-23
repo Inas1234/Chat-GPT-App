10fea71f3f8af0b2a87fac2c20082bc802b1f4d1
Inas1234 <inas.malkic@yahoo.com>
2022-12-23 20:45:35 +0100

    Why do i keep doing this

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 const configuration = new openai.Configuration({
-  apiKey: "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",
+  apiKey: "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",
 });
 
 const client = new openai.OpenAIApi(configuration);
